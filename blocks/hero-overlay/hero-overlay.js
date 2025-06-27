526868c627e219a5ea684802e29edb89b92d39ba
sarang <sarang.kulkarni@foxtel.com.au>
2025-06-27 11:09:01 +1000

    Updated hero-overlay js

--- blocks/hero-overlay/hero-overlay.js
+++ blocks/hero-overlay/hero-overlay.js
@@ -1,15 +1,17 @@
 import { buildBlock, createOptimizedPicture } from "../../scripts/aem.js";
+import { moveInstrumentation } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-
-  // Create a new div for the hero banner
-  // and append the first child of the block to it
   const heroBanner = document.createElement("div");
+  const heroPicture = block.firstElementChild;
+
   heroBanner.classList.add("hero-banner");
-  heroBanner.append(block.firstElementChild);
+  moveInstrumentation(heroPicture, heroBanner);
+  heroBanner.append(heroPicture);
+
   heroBanner.firstElementChild.classList.add("hero-banner--elements");
 
-   // Add a class to the first child picture element
+  // Add a class to the first child picture element
   const firstPicture = heroBanner.firstElementChild.querySelector("picture");
   if (firstPicture) {
     firstPicture.classList.add("hero-banner--picture");
@@ -17,21 +19,20 @@ export default function decorate(block) {
 
   // Create a new div for the hero overlay
   const heroBannerTexts = document.createElement("div");
-  heroBannerTexts.classList.add("hero-overlay");
+  heroBannerTexts.classList.add("hero-overlay-block");
 
-   // Move overlay content into heroBannerTexts
+  // Move overlay content into heroBannerTexts
   [...block.children].slice(0, 3).forEach((child) => {
     if (child) {
       if (!child.querySelector("picture") && child.querySelector("p")) {
         child.classList.add("hero-overlay--text");
       }
+      moveInstrumentation(child, heroBannerTexts);
       heroBannerTexts.append(child);
     }
   });
 
-  const heroBannerElements = heroBanner.querySelector(
-    ".hero-banner--elements"
-  );
+  const heroBannerElements = heroBanner.querySelector(".hero-banner--elements");
 
   // Append overlay as sibling to picture inside hero-banner--elements
   if (heroBannerElements) {
@@ -46,13 +47,11 @@ export default function decorate(block) {
 
   heroBanner.append(heroBannerElements);
 
-   // create new block with the heroBanner
+  // create new block with the heroBanner
   const heroOverlayBlock = buildBlock("hero-banner-block", {
     elems: [heroBanner],
   });
 
-  block.replaceWith(heroOverlayBlock);
-
-  console.log(heroBanner);
-  console.log(block);
+  block.append(heroBanner);
+  console.log("Hero Overlay Block", block);
 }
