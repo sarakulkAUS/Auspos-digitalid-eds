3e7d07401d39226865a2480396f4265dd061c19d
sarang <sarang.kulkarni@foxtel.com.au>
2025-07-02 13:00:43 +1000

    Added styles to columns for cards with icon

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,4 @@
 export default function decorate(block) {
-console.log('Decorating columns block:', block);
 
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
