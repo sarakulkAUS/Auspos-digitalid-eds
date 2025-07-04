d76523aaf6d40fdcc4a138a6c1d5f2e44262786c
sarang <sarang.kulkarni@foxtel.com.au>
2025-07-04 13:19:31 +1000

    added quicklinks styles

--- blocks/quicklinks/quicklinks.js
+++ blocks/quicklinks/quicklinks.js
@@ -1,25 +1,6 @@
 import { moveInstrumentation } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-    const title = block.firstElementChild;
-    title.classList.add("quicklink-title");
-
-    const ul = document.createElement("ul");
-    const li = '';
-    ul.classList.add("links");
-
-    [...block.children].slice(1).forEach((child) => {
-        li = document.createElement("li");
-        li.classList.add("link");
-        moveInstrumentation(child, li);
-        li.append(child);
-        console.log("child", li);
-    });
-
-    ul.append(li);
-    console.log("ul", ul);
-
-    console.log(title);
-    console.log(block);
-
+    const title = block.querySelector("div > p");
+    title.classList.add("title");
 }
